6eab7979a61a2f0be3f82305cea27e6f49e84d62
lenacharutha <lenacharutha@gmail.com>
2014-08-07 00:08:32 -0700

    kjhlhlkklupdate

--- DisneyPrincess/src/HelloPrincess.java
+++ DisneyPrincess/src/HelloPrincess.java
@@ -2,6 +2,27 @@
 public class HelloPrincess {
 public static void main(String args[])
 {
-System.out.println("Hello from Princess");
+int itemnumber =1406;
+//System.out.println("Hello from Princess, +  age");
+
+char grade;
+
+if (itemnumber >=1390) {
+    grade = 'A';
+   // DancePrincess.dPrincess();
+    System.out.println("Ship to Newyork for This xmas");
+   
+} else if(itemnumber >=1100)  {
+    grade = 'B';
+    System.out.println("Ship to Los Angeles for Thanksgiving");
+     
+} 
+else 
+{
+	 grade = 'C';
+System.out.println("Ship to Walmart Bentonville");
+}
+System.out.println("This Doll is of Grade " + grade);
+
 }
 }
